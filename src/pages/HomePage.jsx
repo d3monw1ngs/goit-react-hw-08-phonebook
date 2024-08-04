bd942afc70ca6291bac970de21e1d8233ebe0b41
d3monw1ngs <tezzielumsit87@gmail.com>
2024-08-04 11:12:44 +0800

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Phonebook App</h1>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1 style={{ color: 'blue', fontSize: '24px' }}>Welcome to the Phonebook App</h1>
     </div>
   );
 };
